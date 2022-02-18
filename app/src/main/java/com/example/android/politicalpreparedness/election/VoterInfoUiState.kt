60a2b364d503fa9f11afd66a397801adfceccba8
toelie <toelie.cumlm.cs@gmail.com>
2022-02-18 11:58:17 +0630

    Polish voter info screen

--- app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoUiState.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/election/VoterInfoUiState.kt
@@ -4,16 +4,18 @@ import com.example.android.politicalpreparedness.models.Address
 import com.example.android.politicalpreparedness.models.Election
 
 data class VoterInfoUiState(
+    val isLoading: Boolean = false,
     val election: Election? = null,
     val votingLocationFinderUrl: String = "",
     val ballotInfoUrl: String = "",
     val address: Address? = null,
     val isFollowing: Boolean = false
 ) {
-
+    val showDate: Boolean = election != null
     val showVotingLocation: Boolean = votingLocationFinderUrl.isNotEmpty()
     val showBallotInfo: Boolean = ballotInfoUrl.isNotEmpty()
     val showAddress: Boolean = address != null
+    val hasElectionInformation: Boolean = showVotingLocation || showBallotInfo || showAddress
 
     companion object {
         val Empty = VoterInfoUiState()
